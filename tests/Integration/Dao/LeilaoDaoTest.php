70456517b4854e0530c4081b7bd1df97bec86904
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-23 15:26:05 -0300

    Refactor with PHPUnit tearDown

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -9,12 +9,18 @@ use PHPUnit\Framework\TestCase;
 
 class LeilaoDaoTest extends TestCase
 {
+    private $pdo;
+
+    public function setUp(): void
+    {
+        $this->pdo = ConnectionCreator::getConnection();
+    }
+
     public function testInsercaoEBuscaDevemFuncionar()
     {
         // Arrange - Given
         $leilao = new Leilao('Variant 0Km');
-        $pdo = ConnectionCreator::getConnection();
-        $leilaoDao = new LeilaoDao($pdo);
+        $leilaoDao = new LeilaoDao($this->pdo);
 
         $leilaoDao->salva($leilao);
         // Act -When
@@ -23,9 +29,11 @@ class LeilaoDaoTest extends TestCase
         // Assert - Then
         self::assertCount(1, $leiloes);
         self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
-        self::assertSame('Variant 0Km', $leiloes[0]->recuperarDescricao());
-        
-        // Tear down
-        $pdo->exec('DELETE FROM leiloes');
+        self::assertSame('Variant 0Km', $leiloes[0]->recuperarDescricao());        
+    }
+
+    public function tearDown(): void
+    {
+        $this->pdo->exec('DELETE FROM leiloes');
     }
 }
