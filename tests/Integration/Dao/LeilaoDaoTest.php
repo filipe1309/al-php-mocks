eb0119b980bab1d6c51d10edafd7954c5f2bfd26
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-23 14:53:25 -0300

    Add a code to clean base after test

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -11,14 +11,21 @@ class LeilaoDaoTest extends TestCase
 {
     public function testInsercaoEBuscaDevemFuncionar()
     {
+        // Arrange - Given
         $leilao = new Leilao('Variant 0Km');
-        $leilaoDao = new LeilaoDao(ConnectionCreator::getConnection());
+        $pdo = ConnectionCreator::getConnection();
+        $leilaoDao = new LeilaoDao($pdo);
 
         $leilaoDao->salva($leilao);
+        // Act -When
         $leiloes = $leilaoDao->recuperarNaoFinalizados();
         
+        // Assert - Then
         self::assertCount(1, $leiloes);
         self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
         self::assertSame('Variant 0Km', $leiloes[0]->recuperarDescricao());
+        
+        // Tear down
+        $pdo->exec('DELETE FROM leiloes');
     }
 }
